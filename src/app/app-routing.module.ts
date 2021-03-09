8de296d783e3ae778f8b02a6620507660ce24de0
A-Brenner <abrenner19@gmail.com>
2021-03-09 13:01:18 -0500

    Industries component, logo resized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { IndustriesComponent } from './industries/industries.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'industries', component: IndustriesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
