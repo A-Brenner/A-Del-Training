ac6fc4007eed87e43a4f2eb8a83c47be8c4678ab
A-Brenner <abrenner19@gmail.com>
2021-03-09 14:43:02 -0500

    Begin Training Btn Works :)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title: String = 'A-Del Training';
+  constructor(private router: Router) {}
 
-  constructor() {}
+  // onClick Method
+  // Takes user to the Industries page
+  beginTrainingClick() {
+    this.router.navigateByUrl('/industries');
+  }
 
   ngOnInit(): void {}
 }
