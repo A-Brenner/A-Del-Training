5f7274bc384e1fadd7e42705ecf2bfcd198062a4
A-Brenner <abrenner19@gmail.com>
2021-03-13 14:26:51 -0500

    sections component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IndustriesComponent } from './industries/industries.component';
+import { SectionsComponent } from './sections/sections.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IndustriesComponent
+    IndustriesComponent,
+    SectionsComponent
   ],
   imports: [
     BrowserModule,
