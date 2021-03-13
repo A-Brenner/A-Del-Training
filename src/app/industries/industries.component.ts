208477d93ad336b2e75e4121ee18504cf3bcfa1e
A-Brenner <abrenner19@gmail.com>
2021-03-13 13:33:03 -0500

    onClick Methods set in place for labels & images

--- src/app/industries/industries.component.ts
+++ src/app/industries/industries.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-industries',
   templateUrl: './industries.component.html',
-  styleUrls: ['./industries.component.scss']
+  styleUrls: ['./industries.component.scss'],
 })
 export class IndustriesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // OnCLick methods for images and labels
+  newEmployeesClicked() {
+    console.log('new employees');
   }
 
+  fieldWorkersClicked() {
+    console.log('field workers');
+  }
+
+  shopWorkersClicked() {
+    console.log('shop workers & mechanics');
+  }
+
+  officeEmployeesClicked() {
+    console.log('office employees');
+  }
+
+  foremanClicked() {
+    console.log('foreman');
+  }
+
+  truckDriversClicked() {
+    console.log('truck drivers');
+  }
+  ngOnInit(): void {}
 }
