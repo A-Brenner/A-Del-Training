cce8a93fe102b8b17a11cc35ed07848100641a95
A-Brenner <abrenner19@gmail.com>
2021-03-13 14:28:47 -0500

    route and path added for sections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IndustriesComponent } from './industries/industries.component';
+import { SectionsComponent } from './sections/sections.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'training-programs', component: IndustriesComponent },
+  { path: 'training-programs/sections', component: SectionsComponent },
 ];
 
 @NgModule({
