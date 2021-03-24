cf3326c8de57e0479a5e2b7ad2e80c4b5edf2056
A-Brenner <abrenner19@gmail.com>
2021-03-24 12:31:43 -0400

    section titles & links added for Shop Workers

--- src/app/sections/sections.component.ts
+++ src/app/sections/sections.component.ts
@@ -34,6 +34,8 @@ export class SectionsComponent implements OnInit {
       }
       case 'shopWorkers': {
         this.trainingProgram = 'Shop Workers & Mechanics';
+        this.createSectionsSW();
+        console.log(this.sectionsArr);
         break;
       }
       case 'officeEmployees': {
@@ -67,7 +69,7 @@ export class SectionsComponent implements OnInit {
   // NEW EMPLOYEES
   // Creates sections for New Employees, Adds sections to array
   createSectionsNE(): void {
-    // array of New Employee section titles
+    // array of section titles
     let titles: string[] = [
       'EEO',
       'Safety Orientation',
@@ -75,7 +77,7 @@ export class SectionsComponent implements OnInit {
       'Cell Phones',
       'Drugs & Alcohol',
     ];
-    // array of New Employee video links
+    // array of video links
     let links: string[] = [
       '', // (Audra) EEO
       'https://safetysourceonline.com/video/safety-bobs-comprehensive-construction-orientation-e1316e-24-min-2/',
@@ -98,14 +100,14 @@ export class SectionsComponent implements OnInit {
   // OFFICE EMPLOYEES
   // Creates sections for Office Employees, Adds sections to array
   createSectionsOE(): void {
-    // array of New Employee section titles
+    // array of section titles
     let titles: string[] = [
       'Emergency Action Plan',
       'Surviving an Active Shooter',
       'Sexual Harassment',
       'First Aid',
     ];
-    // array of New Employee video links
+    // array of video links
     let links: string[] = [
       '', // (Chuck) Emergency Action Plan
       'https://www.youtube.com/watch?v=DFQ-oxhdFjE',
@@ -127,7 +129,7 @@ export class SectionsComponent implements OnInit {
   // FIELD WORKERS
   // Creates sections for Field Workers, Adds sections to array
   createSectionsFW(startIndex: number): void {
-    // array of New Employee section titles
+    // array of section titles
     let titles: string[] = [
       'PPE: Basic Training',
       'Hazardous Materials Labels',
@@ -151,7 +153,7 @@ export class SectionsComponent implements OnInit {
       'Aerial Lift Safety',
       'Working Around Equipment',
     ];
-    // array of New Employee video links
+    // array of video links
     let links: string[] = [
       'https://safetysourceonline.com/video/ppebasic-training-1028b-12-min/',
       'https://safetysourceonline.com/video/ghs-labels-ss2001fe/',
@@ -167,7 +169,7 @@ export class SectionsComponent implements OnInit {
       'https://safetysourceonline.com/video/confined-space-entry-ss1055he-10-min/',
       '', // small tools
       'https://safetysourceonline.com/video/crystalline-silica-safety/',
-      'https://safetysourceonline.com/video/machine-guarding-conveyor-safety-1003h-12-min/',
+      'https://safetysourceonline.com/video/grinding-and-abrasive-wheels-ss040789/',
       'https://safetysourceonline.com/video/choices-safe-driving-1078ie/',
       '', // rigging & load securement
       'https://safetysourceonline.com/video/hand-power-tool-safety-ss1094ie-10-min/',
@@ -190,15 +192,15 @@ export class SectionsComponent implements OnInit {
   // FOREMAN
   // Creates sections for Foreman, Adds sections to array
   createSectionsFM(): void {
-    // array of New Employee section titles
+    // array of section titles
     let titles: string[] = [
       'Equipment',
       'Accident Investigation',
       'Lock Out Tag Out',
-      'Drug & Alcohol Supervisor',
+      'Drug & Alcohol - Supervisors',
       'HCSS Reporting',
     ];
-    // array of New Employee video links
+    // array of video links
     let links: string[] = [
       '', // Equipment
       'https://safetysourceonline.com/video/accident-investigation-for-everyone-2485-2/',
@@ -219,10 +221,69 @@ export class SectionsComponent implements OnInit {
     }
   }
 
+  // SHOP WORKERS & MECHANICS
+  // Creates sections for Shop Workers, Adds sections to array
+  createSectionsSW(): void {
+    // array of section titles
+    let titles: string[] = [
+      'PPE: Basic Training',
+      'Hazardous Materials Labels',
+      'GHS: Safety Data Sheets',
+      'Ladder Safety',
+      'Sexual Harassment',
+      'Fire Prevention',
+      'First Aid',
+      'Basic Electrical Safety',
+      'Machine Guarding',
+      'Hand & Power Tools',
+      'Aerial Lift Safety',
+      'Forklift Safety',
+      'Welding & Cutting',
+      'Arc Flash',
+      'Compressed Gas Cylinders',
+      'Lock Out Tag Out',
+      'Making Safety Work',
+      'Heat Stress',
+      'Slips, Trips, & Falls',
+    ];
+    // array of video links
+    let links: string[] = [
+      'https://safetysourceonline.com/video/ppebasic-training-1028b-12-min/',
+      'https://safetysourceonline.com/video/ghs-labels-ss2001fe/',
+      'https://safetysourceonline.com/video/ghs-safety-data-sheets-the-basics-ss2002fe/',
+      'https://safetysourceonline.com/video/ladder-safety-8019a-10-min/',
+      '', // sexual harassment
+      'https://safetysourceonline.com/video/to-the-point-about-fire-prevention-response-tp07/',
+      'https://safetysourceonline.com/video/first-aid-m209/',
+      'https://safetysourceonline.com/video/basic-electrical-safety-1085i-11-min/',
+      'https://safetysourceonline.com/video/grinding-and-abrasive-wheels-ss040789/',
+      'https://safetysourceonline.com/video/hand-power-tool-safety-ss1094ie-10-min/',
+      'https://safetysourceonline.com/video/aerial-lift-safety-ss1031be/',
+      'https://safetysourceonline.com/video/to-the-point-about-safe-forklift-operation-tp09-11-min/',
+      'https://safetysourceonline.com/video/creating-safety-in-welding-operations-4760/',
+      'https://safetysourceonline.com/video/arc-flash-updated/',
+      'https://safetysourceonline.com/video/compressed-gas-cylinders-m259e/',
+      '', // Lock out tag out
+      '', // Making safety work
+      'https://safetysourceonline.com/video/heat-stress-facts-and-prevention/',
+      'https://safetysourceonline.com/video/slips-trips-falls-ss1064ie-5-concise-version/',
+    ];
+
+    for (let i = 0; i < titles.length; i++) {
+      let section: sectionModule.Section = new sectionModule.Section(
+        i + 1,
+        titles[i],
+        false,
+        links[i]
+      );
+      this.sectionsArr.push(section);
+    }
+  }
+
   // TRUCK DRIVERS
   // Creates sections for TRUCK DRIVERS, Adds sections to array
   createSectionsTD(): void {
-    // array of New Employee section titles
+    // array of section titles
     let titles: string[] = [
       'PPE: Basic Training',
       'Hazardous Materials Labels',
@@ -236,7 +297,7 @@ export class SectionsComponent implements OnInit {
       'Dump Truck Safety',
       'Driver Safety',
     ];
-    // array of New Employee video links
+    // array of video links
     let links: string[] = [
       'https://safetysourceonline.com/video/ppebasic-training-1028b-12-min/',
       'https://safetysourceonline.com/video/ghs-labels-ss2001fe/',
